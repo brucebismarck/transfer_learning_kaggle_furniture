46d230af18f819b00c1c01ea3d604844505e34b4
Bismarck Liu <brucebismarck@users.noreply.github.com>
2018-05-31 00:05:09 -0400

    convert json to csv

--- src/download.py
+++ src/download.py
@@ -1,102 +1,65 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  1 16:08:14 2018
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+import csv
+import os
+from pandas import DataFrame
 
-@author: bruce
-"""
+import urllib
 
-import os, multiprocessing, urllib3
-from PIL import Image
-from io import BytesIO
-import pandas as pd
-#from itertools import repeat
-from tqdm import tqdm
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+os.chdir("E:\\Data playground\\kaggle_furniture\\")
 
-def DownloadImage(key_url):
-  out_dir = "/home/wenyue/Desktop/data_playground/kaggle_furniture/test_1"
-  current_dir = "/home/wenyue/Desktop/data_playground/kaggle_furniture/test"
-  (key, url) = key_url
-  
-  filename = os.path.join(current_dir, '%s.jpg' % key)
+with open("train.json") as datafile1: #first check if it's a valid json file or not
+    data1 = json.load(datafile1)
+with open("test.json") as datafile2: #first check if it's a valid json file or not
+    data2 = json.load(datafile2)
+with open("validation.json") as datafile3: #first check if it's a valid json file or not
+    data3 = json.load(datafile3)
 
-  if os.path.exists(filename):
-    print('Image %s already exists. Skip downloading.' % filename)
-    return 
-    
-  try:
-    #print('Trying to get %s.' % url)
-    http = urllib3.PoolManager()
-    response = http.request('GET', url, timeout = 5)
-    image_data = response.data
-  except:
-    print('Warning: Could not download image %s from %s' % (key, url))
-    output = open(out_dir + 'error.log', 'a')
-    output.write('Warning: Could not download image %s from %s' % (key, url) + '\n')
-    output.close()
-    return 
+datafile1.close()
+datafile2.close()
+datafile3.close()
 
-  try:
-    pil_image = Image.open(BytesIO(image_data))
-  except:
-    print('Warning: Failed to parse image %s %s' % (key,url))
-    output = open(out_dir + 'error.log', 'a')
-    output.write('Warning: Failed to parse image %s %s' % (key,url)+ '\n')
-    output.close()
-    return 
 
-  try:
-    pil_image_rgb = pil_image.convert('RGB')
-  except:
-    print('Warning: Failed to convert image %s to RGB' % key)
-    output = open(out_dir + 'error.log', 'a')
-    output.write('Warning: Failed to convert image %s to RGB' % key + '\n')
-    output.close()
-    return 
+# for training data
+my_dic_data = data1
+keys= my_dic_data.keys()
+dict_you_want1={'my_items1':my_dic_data['annotations']for key in keys}
+dict_you_want2={'my_items2':my_dic_data['images']for key in keys}
+df=pd.DataFrame(dict_you_want1)
+fd = pd.DataFrame(dict_you_want2)
+df2=df['my_items1'].apply(pd.Series)
+fd2=fd['my_items2'].apply(pd.Series)
+train_data = pd.merge(df2, fd2, on='image_id', how='outer')
 
-  try:
-    pil_image_rgb.save(filename, format='JPEG', quality=90)
-  except:
-    print('Warning: Failed to save image %s' % filename)
-    output = open(out_dir + 'error.log', 'a')
-    output.write('Warning: Failed to save image %s' % filename + '\n')
-    output.close()
-    return 
+# for validation data
+my_dic_data = data3
+keys= my_dic_data.keys()
+dict_you_want1={'my_items1':my_dic_data['annotations']for key in keys}
+dict_you_want2={'my_items2':my_dic_data['images']for key in keys}
+df=pd.DataFrame(dict_you_want1)
+fd = pd.DataFrame(dict_you_want2)
+df2=df['my_items1'].apply(pd.Series)
+#print ("df2",df2.head())
+fd2=fd['my_items2'].apply(pd.Series)
+#print ("fd2",fd2.head())
+validation_data = pd.merge(df2, fd2, on='image_id', how='outer')
 
-       
-def multi_downloader(csv_file):
-    data  = pd.read_csv(csv_file,index_col = 0)
-    try:
-        data_new = data.drop('label_id', 1)
-    except ValueError:
-        data_new = data
-    
-    key_url_list = [tuple(x) for x in data_new.values]
-                    
-                    
-    pool = multiprocessing.Pool(40)
-    length = len(key_url_list)
-    
-    with tqdm(total = length) as t :
-        for _ in pool.imap_unordered(DownloadImage, key_url_list):
-            t.update(1)
-            
-if __name__ == '__main__':
-    multi_downloader("/home/wenyue/Desktop/data_playground/kaggle_furniture/test_data.csv")
- 
+# for test data
+my_dic_data = data2
+keys= my_dic_data.keys()
+dict_you_want2={'my_items2':my_dic_data['images']for key in keys}
+fd = pd.DataFrame(dict_you_want2)
+test_data=fd['my_items2'].apply(pd.Series)
 
-data = pd.read_csv("/home/wenyue/Desktop/data_playground/kaggle_furniture/test_data.csv",index_col = 0)
-exist_pic = os.listdir('/home/wenyue/Desktop/data_playground/kaggle_furniture/test')
-num_list = [int(item.split('.')[0]) for item in exist_pic]
+train_data['url'] = train_data['url'].apply(lambda x:str(x[0]))
+test_data['url'] = test_data['url'].apply(lambda x:str(x[0]))
+validation_data['url'] = validation_data['url'].apply(lambda x:str(x[0]))
 
-data = data[data['image_id'].isin(num_list) == False]      
 
-key_url_list = [tuple(x) for x in data.values]
-
-pool = multiprocessing.Pool(40)
-length = len(key_url_list)
-    
-with tqdm(total = length) as t :
-    for _ in pool.imap_unordered(DownloadImage, key_url_list):
-        t.update(1)
+train_data.to_csv('train_data.csv')
+test_data.to_csv('test_data.csv')
+validation_data.to_csv('validation_data.csv')
